a5956c469d315bddae06911f9411dc2bb7238509
RokkaBaba <60067118+RokkaBaba@users.noreply.github.com>
2025-01-30 22:43:26 -0500

    Update area_of_circle.py

--- Beginner Exercises/area_of_circle.py
+++ Beginner Exercises/area_of_circle.py
@@ -1,1 +1,11 @@
+# import pi which is a constant from math module
+from math import pi 
 
+#  user to input radius of circle
+r = float(input("Input the radius of Circle:"))
+
+# area =  π * r^2
+area = pi * r** 2 
+
+# display the result
+print ("The area of the circle with radius"+ str(r)+"is:"+str(area))
