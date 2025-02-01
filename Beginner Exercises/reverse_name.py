9335894fc3ba61bb0dfde6c82deb9f0be4cbff85
RokkaBaba <60067118+RokkaBaba@users.noreply.github.com>
2025-01-31 22:12:09 -0500

    Create reverse_name.py

---
+++ Beginner Exercises/reverse_name.py
@@ -0,0 +1,7 @@
+# Print first and last name in reverse order with a space between them
+
+# enter user first-name
+fname = input ("Enter Your First Name")
+# enter user second-name
+lname = input ("Enter Your Last Name")
+print ("Hi"  +lname  +fname)
