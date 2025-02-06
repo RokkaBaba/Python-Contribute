9e790a7c1902fbe8a484c13252f7404fcaf0c10d
RokkaBaba <60067118+RokkaBaba@users.noreply.github.com>
2025-02-05 20:54:44 -0500

    Update file_Ext.py

--- Beginner Exercises/file_Ext.py
+++ Beginner Exercises/file_Ext.py
@@ -1,1 +1,5 @@
+#Write a Python program that accepts a filename from the user and prints the extension of the file.
+filename = input("Input the filename:")
+f_ext=filename.split(".")
+print("The extension of the file is:" + repr(f_ext[-1])) #repr is machine friendly than str
 
